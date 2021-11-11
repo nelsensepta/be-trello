121064400249c51e5389ce60e6c0d50615d4a06c
nelsesepta <nelseseptahenidar@gmail.com>
2021-11-12 03:40:00 +0700

    Add Cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const todosRouter = require("./app/api/todos/router");
 const itemsRouter = require("./app/api/items/router");
@@ -13,6 +14,7 @@ const URL = "/api/v1";
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
